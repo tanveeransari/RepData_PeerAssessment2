156bb7d2c12b190a93c0bef3893a365baa121708
Tanveer.Ansari <tanveeransari@hotmail.com>
2015-04-27 19:27:13 -0500

    Done

--- work.R
+++ work.R
@@ -1,64 +1,55 @@
-#================================START REPR RESEARCH PEER ASSIGNMENT 2
-#  must use database to answer the questions below and show the code for your entire analysis.
-#  analysis can consist of tables, figures, or other summaries.
-#
-# Must address the following questions:
-#  1) Across the United States, which types of events (as indicated in the EVTYPE variable)
-#                                       are most harmful with respect to population health?
-#  2) Across the United States, which types of events have the greatest economic consequences?
-#
-# NOTE : The events in the database start in the year 1950 and end in November 2011.
+# NOTE : Events in database start in 1950 end in November 2011.
 # In earlier years  generally fewer events recorded,  due to a lack of good records.
 # More recent years should be considered more complete.
-#
-#
-# Consider writing your report as if it were to be read by a government or municipal manager
 require(knitr);
-require(data.table); require(plyr);require(dplyr);require(tidyr);require(ggplot2);require(reshape2);
-library(data.table); library(plyr);library(dplyr);library(tidyr);library(ggplot2);library(reshape2);
-
-
+#require(data.table); require(plyr);require(tidyr);
+require(dplyr);require(ggplot2);require(reshape2);
+library(dplyr);library(ggplot2);library(reshape2);
+#library(data.table); library(tidyr);library(plyr);
 # download.file
 if(!file.exists("stormdata.csv.bz2")) {
   download.file("https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2",
                 destfile = "stormdata.csv.bz2")
 }
-## READ raw data from file
-#storm <- read.csv("stormdata.csv.bz2",stringsAsFactors=F)
-raw<-read.csv("stormdata.csv.bz2",stringsAsFactors=F,na.strings=c("NA,NAN",""))
-storm<-tbl_df(raw)
-#rm(raw)
 
+## READ raw data from file
+storm<-read.csv("stormdata.csv.bz2",stringsAsFactors=F,na.strings=c("NA,NAN",""))
+storm<-tbl_df(storm)
+
+#  1) Across the United States, which types of events are most harmful with respect to population health?
 ## Only select those events with either fatalities or injuries
 injuries=subset(storm, FATALITIES>0 | INJURIES>0)
 ##  Aggregate data by FATALITIES & INJURIES
 #inj<-aggregate(cbind(FATALITIES,INJURIES)~EVTYPE, data=storm,sum)
-inj<-injuries%>% group_by(EVTYPE)%>%summarize(killed=sum(FATALITIES,na.rm = T),hurt=sum(INJURIES,na.rm=T))
-#inj<-arrange(t,desc(killed),desc(hurt))
+inj<-injuries%>% group_by(EVTYPE)%>%summarize(fatalities=sum(FATALITIES,na.rm = T),hurt=sum(INJURIES,na.rm=T))
+#inj<-arrange(t,desc(fatalities),desc(hurt))
 # find top 10% of fatalities
-quantile(inj$killed,prob=seq(0.95,1,by=0.01))
-#Gives us 208 as 99th percentile - select event types killed more than 200 people in total?
-inj<-subset(inj,killed>500)
-inj<-arrange(inj,desc(killed),desc(hurt))
+quantile(inj$fatalities,prob=seq(0.95,1,by=0.01))
+#Gives us 208 as 99th percentile - select event types fatalities more than 200 people in total?
+inj<-subset(inj,fatalities>500)
+#Examine them
+arrange(inj,desc(fatalities),desc(hurt))
+#Order the eventtypes as a factor with levels in descending order of fatalities(existing order of data).
+# This will make plot ordered by fatalities, on x-axis
+inj$EVTYPE=factor(inj$EVTYPE,levels=inj$EVTYPE[order(desc(inj$fatalities))])
 ## Plot top harmful events
-injmelt<-melt(inj,id=c("EVTYPE"),measure.vars=c("killed","hurt"))
+injmelt<-melt(inj,id=c("EVTYPE"),measure.vars=c("fatalities","hurt"))
 #qplot(EVTYPE,value,data=injmelt,facets=variable~.,color=EVTYPE)
 
-ggplot(injmelt,aes(x=EVTYPE,y=value,fill=factor(variable)))
-+geom_bar(position="dodge",stat="identity")+ggtitle("Storm Events causing Top 6 Fatalities")
+ggplot(injmelt,aes(x=EVTYPE,y=value,fill=factor(variable)))+geom_bar(position="dodge",stat="identity")+ggtitle(
+  "Storm Events causing Top 6 Fatalities")
 
-###ANALYZE TOP ECONOMIC CONSEQUENCES
+#  2) Across the United States, which types of events have the greatest economic consequences?
 ## only select those events with either property or crop damage
-stormdmg<-subset(storm,PROPDMG>0&!is.na(PROPDMGEXP)|(CROPDMG>0&!is.na(CROPDMGEXP)))
+stormdmg<-subset(storm,(PROPDMG>0&!is.na(PROPDMGEXP))|(CROPDMG>0&!is.na(CROPDMGEXP)))
 ## take only columns we are interested in
 # stormdmg<-select(stormdmg,EVTYPE,PROPDMG,PROPDMGEXP,CROPDMG,CROPDMGEXP,STATE,BGN_DATE,BGN_TIME,TIME_ZONE)
-stormdmg<-select(stormdmg,EVTYPE,PROPDMG,PROPDMGEXP,CROPDMG,CROdPDMGEXP)
+stormdmg<-select(stormdmg,EVTYPE,PROPDMG,PROPDMGEXP,CROPDMG,CROPDMGEXP)
 #Some of the above suffixes appear to be invalid.
 sort(unique(c(unique(stormdmg$CROPDMGEXP),unique(stormdmg$PROPDMGEXP))))
 ## Only consider damanges in either thousands, millions or billions
 validDamage<-c("K","k","M","m","B","b")
 hdmg<-filter(stormdmg,PROPDMGEXP %in% validDamage | CROPDMGEXP %in% validDamage)
-#expvals<-as.vector(sort(unique(c(unique(hdmg$CROPDMGEXP),unique(hdmg$PROPDMGEXP)))))
 
 if(any(is.na(hdmg$PROPDMGEXP))) hdmg[which(is.na(hdmg$PROPDMGEXP)),]$PROPDMGEXP=0
 hdmg$PROPDMGEXP<-gsub("[Kk]",as.character(10^3),hdmg$PROPDMGEXP)
@@ -77,11 +68,15 @@ hdmg$CROPDMGEXP<-gsub("\\?",as.character(0),hdmg$CROPDMGEXP)
 
 #Lets calculate the numbers in dollars and the total damage per row
 hdmg<-mutate(hdmg,prop=PROPDMG*as.numeric(PROPDMGEXP),crop=CROPDMG*as.numeric(CROPDMGEXP),total=crop+prop)
-# Now lets summarize and order by TotalDamage Desc
+# Now lets summarize and order by TotalDamage Desc, then take the top 6
 aggdmg<-hdmg%>%group_by(EVTYPE)%>%
   summarize(PropertyDamage=sum(prop),CropDamage=sum(crop),TotalDamage=sum(total))%>%arrange(desc(TotalDamage))
-#Take the top six
 aggdmg<-aggdmg[1:6,]
+
+#Order the eventtypes as a factor with levels in descending order of total damage (existing order of data).
+#This will make our plot ordered by fatalities, highest first
+aggdmg$EVTYPE=factor(aggdmg$EVTYPE,levels=aggdmg$EVTYPE[order(desc(aggdmg$TotalDamage))])
+
 #Now we don't need the total as we are done sorting
 aggdmg$TotalDamage<-NULL
 #Lets melt the data together for plotting
